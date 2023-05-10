b533bad642ea7ca75b17abc574d81268c84434e7
Caroline Faria <caroline.gdfaria@gmail.com>
2023-05-10 18:53:34 -0300

    escondendo informações sensíveis

--- bancoDeDados.js
+++ bancoDeDados.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose')
+require('dotenv').config()
 
 async function conectaBancoDeDados() {
   try {
     console.log('conexão com o banco de dados inciou')
 
-    await mongoose.connect('mongodb+srv://carolinegdfaria:Kbq2u5UML5rrVGXH@clustermulheres.bhmb6zc.mongodb.net/?retryWrites=true&w=majority')
+    await mongoose.connect(process.env.MONGO_URL)
 
     console.log('conexão com o banco de dados feita com sucesso!')
   } catch(erro) {
